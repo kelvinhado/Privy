f541149ebcfda9e06e556133aff804f7128600f6
kelvinhado <kelvinhado@outlook.com>
2017-10-01 11:13:35 +0200

    add fragment view

--- app/src/main/java/com/kelvinhado/privy/privies/PriviesMapFragment.java
+++ app/src/main/java/com/kelvinhado/privy/privies/PriviesMapFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.kelvinhado.privy.R;
 import com.kelvinhado.privy.data.Privy;
 
 import java.util.List;
@@ -29,6 +33,14 @@ public class PriviesMapFragment extends Fragment implements PriviesContract.View
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_privies_map, container, false);
+        return root;
     }
 
     @Override
