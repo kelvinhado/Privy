4ca218b345892a4c2d11922d3eb795545943ab80
kelvinhado <kelvinhado@outlook.com>
2017-10-01 13:11:33 +0200

    add fab add butterknife switch to coordinator layout

--- app/src/main/java/com/kelvinhado/privy/privies/PriviesActivity.java
+++ app/src/main/java/com/kelvinhado/privy/privies/PriviesActivity.java
@@ -2,10 +2,14 @@ package com.kelvinhado.privy.privies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.kelvinhado.privy.R;
 import com.kelvinhado.privy.data.Injection;
 import com.kelvinhado.privy.utils.ActivityUtils;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class PriviesActivity extends AppCompatActivity {
 
@@ -13,7 +17,7 @@ public class PriviesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privies);
-
+        ButterKnife.bind(this);
         PriviesMapFragment priviesMapFragment =
                 (PriviesMapFragment) getSupportFragmentManager().findFragmentById(R.id.content_frame);
         if (priviesMapFragment == null) {
@@ -24,9 +28,14 @@ public class PriviesActivity extends AppCompatActivity {
         }
 
         // add presenter
-        PriviesPresenter presenter = new PriviesPresenter(
+        new PriviesPresenter(
                 Injection.providePriviesRepository(this),
                 priviesMapFragment);
 
+    }
+
+    @OnClick(R.id.fab)
+    public void navigateToList() {
+        Toast.makeText(this, "go to list", Toast.LENGTH_SHORT).show();
     }
 }
